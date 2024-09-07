2c8648b49ec38531dee838d09d23f87ff72c4f6d
Ayano Fujita <cathy.ayano@gmail.com>
2024-09-07 21:10:42 +0900

    semicolons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
 import SearchBar from "./Searchbar/Searchbar";
 
 function Navbar({setCafes}) {
